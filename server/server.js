8767e368f81fd6f0d1799aa9b74ea46e8aea75ad
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-06-15 17:09:30 +0200

    config server package.json

--- server/server.js
+++ server/server.js
@@ -4,3 +4,13 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
+
+import cors from "cors";
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
