9429fbec37917a026b4df1f09b335780cda73c7a
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-06-16 17:16:45 +0200

    init schema+model

--- server/routes/lakes.js
+++ server/routes/lakes.js
@@ -1,7 +1,20 @@
 import express from "express";
+import Lake from "../models/lakesModel.js";
+
 const router = express.Router();
 
-router.get("/test", (req, res) => {
+/* router.get("/test", (req, res) => {
   res.send({ msg: "Test route." });
+}); */
+
+router.get("/all", (req, res) => {
+  Lake.find({}, function (err, lakes) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(lakes);
+    }
+  });
 });
+
 export default router;
--- server/server.js
+++ server/server.js
@@ -5,17 +5,45 @@ import * as dotenv from "dotenv";
 import lakes from "./routes/lakes.js";
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("Server is running on " + port + "port");
-});
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
+const startServer = () => {
+  app.listen(port, () => {
+    console.log("Server is running on " + port + "port");
+  });
+};
 
-app.use("/lakes", lakes);
+const loadRoutes = () => {
+  app.use("/lakes", lakes);
+};
+
+const middlewareSetup = () => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  const corsOptions = {
+    origin: "http://localhost:3000", // or '*'
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+};
+
+const mongoDbConection = async () => {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("Connection to Mongo DB established");
+  } catch (error) {
+    console.log("error connection to Mongo DB", err);
+  }
+};
+
+(async () => {
+  mongoDbConection();
+  middlewareSetup();
+  loadRoutes();
+  startServer();
+})();
