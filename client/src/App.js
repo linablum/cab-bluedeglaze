3e4a066f516aa1799170105c413e89de8d4416e7
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-06-16 18:04:19 +0200

    init context

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,6 @@ import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:5000/lakes/all").catch(
-        console.log("error")
-      );
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchData();
-  }, []);
-
   return <div className="App"></div>;
 }
 
---
+++ client/src/context/LakeContext.js
@@ -0,0 +1,32 @@
+import { useState, createContext } from "react";
+
+export const LakeContext = createContext();
+
+export const PlantsContextProvider = (props) => {
+  //  const [details, setDetails] = useState();
+  //  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:5000/lakes/all").catch(
+        console.log("Error")
+      );
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <LakesContext.Provider
+      value={
+        {
+          //        details,
+          //        loading,
+        }
+      }
+    >
+      {props.children}
+    </LakesContext.Provider>
+  );
+};
