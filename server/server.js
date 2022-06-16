41400caa277786a832497fe16fca9558eef37371
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-06-16 10:48:22 +0200

    dotenv

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import lakes from "./routes/lakes.js";
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
-
-import cors from "cors";
 
 app.use(express.json());
 app.use(
@@ -15,4 +18,4 @@ app.use(
 );
 app.use(cors());
 
-//app.use("/lakes", require("./routes/lakesRoute"));
+app.use("/lakes", lakes);
