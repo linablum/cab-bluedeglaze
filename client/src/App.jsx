0373c1e4245922d6689acd077d2062221414aa8f
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-06-24 12:13:04 +0200

    passport pack

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,41 @@
 import "./App.css";
+import { useState } from "react";
+import { getToken } from "./utils/getToken";
+import { useEffect } from "react";
 import Login from "./views/Login";
 //import SignUp from "./views/Signup";
 
 function App() {
+  // move to auth context
+  const [user, setUser] = useState(false);
+
+  const isUserLoggedIn = () => {
+    const token = getToken();
+    console.log(token);
+    if (token) {
+      setUser(true);
+      console.log("user is logged in");
+    } else {
+      setUser(false);
+      console.log("user is NOT logged in");
+    }
+  };
+
+  useEffect(() => {
+    isUserLoggedIn();
+  }, [user]);
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setUser(false);
+  };
+
   return (
     <div className="App">
       <Login />
+      <button onClick={logOut} style={{ backgroundColor: "red" }}>
+        logout
+      </button>
     </div>
   );
 }
--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { getToken } from "../utils/getToken.js";
 
 function Login() {
   const [loginUser, setLoginUser] = useState({});
