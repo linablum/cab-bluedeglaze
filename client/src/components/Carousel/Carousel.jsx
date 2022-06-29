5ad8008e92050b8de224159fab1aed14bcc60f13
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-06-29 12:39:04 +0200

    imges landing page

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,39 +1,33 @@
 import React from "react";
 import "./Carousel.css";
 import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import Pic from "../../assets/undraw_adventure_map_hnin.svg";
+import Pic2 from "../../assets/undraw_couple.svg";
+import Pic3 from "../../assets/undraw_reading.svg";
 
 function LandingCarousel() {
   return (
     <Carousel fade>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
+        <img className="d-block w-100" src={Pic} alt="First slide" />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3>Blue Deglaze</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
+        <img className="d-block w-100" src={Pic2} alt="Second slide" />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h3>Take me to the lakes</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <div>
+            <Button className="btn-1"></Button>
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src={Pic3} alt="Third slide" />
 
         <Carousel.Caption>
           <h3>Third slide label</h3>
